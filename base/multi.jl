3cd7dff3f8ecb19109e555a6abddb56d75b288a5
kshyatt <kshyatt@physics.ucsb.edu>
2016-07-30 08:30:45 -0700

    Add more feedback and fix whitespace

--- base/multi.jl
+++ base/multi.jl
@@ -766,19 +766,19 @@ hash(r::AbstractRemoteRef, h::UInt) = hash(r.whence, hash(r.id, h))
 `Future`s and `RemoteChannel`s are identified by fields:
 
 `where` - refers to the node where the underlying object/storage
-referred by the reference actually exists.
+referred to by the reference actually exists.
 
 `whence` - refers to the node the remote reference was created from.
- Note that this is different from the node where the underlying object 
- referred to actually exists. For example calling `RemoteChannel(2)` 
- from the master process would results in a `where` value of 2 and 
+ Note that this is different from the node where the underlying object
+ referred to actually exists. For example calling `RemoteChannel(2)`
+ from the master process would result in a `where` value of 2 and
  a `whence` value of 1.
 
  `id` is unique across all references created from the worker specified by `whence`.
 
  Taken together,  `whence` and `id` uniquely identify a reference across all workers.
 
- `Base.remoteref_id` is a low-level API which returns a `Base.RRID` 
+ `Base.remoteref_id` is a low-level API which returns a `Base.RRID`
  object that wraps `whence` and `id` values of a remote reference.
 """
 remoteref_id(r::AbstractRemoteRef) = RRID(r.whence, r.id)
